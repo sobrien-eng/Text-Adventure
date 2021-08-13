fe447610cd1ba22c47192ebb4ae393824c95de2b
wpenrod <admin@abstractprogramming.org>
2021-08-13 15:12:53 -0600

    Finished signup page prototype.

---
+++ js/signup.js
@@ -0,0 +1,29 @@
+window.onload = function() {
+    $('#loading').hide();
+};
+
+function signup() {
+    $('#signup').hide();
+    $('#login').hide();
+    $('#loading').show();
+    $("#username").attr('disabled','disabled');
+    $("#password").attr('disabled','disabled');
+    $("#email").attr('disabled','disabled');
+    let settings = {
+        "url": "https://wpcs.xyz/signup",
+        "method": "POST",
+        "timeout": 0,
+        "data": {
+            "username": $("#username").val(),
+            "password": $("#password").val(),
+            "email": $("#email").val()
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        if (response === "OK") {
+            sessionStorage.setItem('prompt-successful-signup', true);
+            location.replace("https://wpcs.xyz/login");
+        }
+    });
+}

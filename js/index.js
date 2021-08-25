dac24cd4799afb3b2c6422501f7117fbb1523369
JeremieCius <80438932+JeremieCius@users.noreply.github.com>
2021-08-25 16:54:43 -0600

    Update index.js part 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+var settings = {
+    "url": "https://wpcs.xyz/api/user?t=" + sessionStorage.getItem('token'),
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+function getUsername() 
+  { 
+    $.ajax(settings).done(function (response) {    
+    username.innerHTML = (response.token);
+    });
+  }
 
+  getUsername();
